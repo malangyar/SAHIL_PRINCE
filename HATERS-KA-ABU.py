a9168704766da87d06ece25cee5cc65d4cafe033
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-08 12:48:19 +0500

    Update HATERS-KA-ABU.py

--- HATERS-KA-ABU.py
+++ HATERS-KA-ABU.py
@@ -4,5 +4,5 @@ os.system('termux-setup-storage')
 print("\033[1;32m WELCOME TO MY TOLL")
 print("\033[1;32m JALNE WALO JALNA NHI")
 print("\033[1;32m  This Tool Is Cooded By Sahil-Prince Haters Papa Bolne Ka Liye Ready Ho Jao")
-import app
+import JANI
 Main()
